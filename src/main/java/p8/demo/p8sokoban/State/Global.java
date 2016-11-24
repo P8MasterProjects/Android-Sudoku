e385d0873e6e8aadc3044d925bc3d55867f5b6d0
Dragon0Spark <thelastblade@live.fr>
2016-11-24 10:37:02 +0100

    Sudoku
    For test this version, you swap the old "src" folder to this one.

---
+++ src/main/java/p8/demo/p8sokoban/State/Global.java
@@ -0,0 +1,20 @@
+package p8.demo.p8sokoban.State;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by IKARUGA on 07/11/2016.
+ */
+
+public class Global {
+    public static Button winscreen;
+    public static Button menubtn;
+    public static TextView numScore;
+    public static int levelstate;
+    public static boolean winsoko = false;
+    public static boolean menu_state = false;
+    public static int hi_score = 0;
+    public static int final_score = 999;
+    public static int final_score2 = 999;
+}
